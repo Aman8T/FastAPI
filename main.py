2a295cdbd390e3663aca03a4d96f6e52c170a481
Aman8T <77984568+Aman8T@users.noreply.github.com>
2024-03-25 19:06:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Any, Optional
-
+import uvicorn
 
 # Load environment variables from .env file (if any)
 load_dotenv()
